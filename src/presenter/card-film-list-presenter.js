f3bdb810e3d4254f98cc264f341361b04bed51d3
Roman Pokkidov <rom.pokidov@gmail.com>
2022-05-12 15:50:51 +0700

    3.9. Революция или эволюция? (часть 2)

--- src/presenter/card-film-list-presenter.js
+++ src/presenter/card-film-list-presenter.js
@@ -4,12 +4,16 @@ import FilmsListContainerView from '../view/films-card-view/films-list-container
 import CardFilmView from '../view/films-card-view/card-film-view';
 import ShowMoreButton from '../view/films-card-view/show-more-button-view';
 import PopupSectionView from '../view/popup-view/popup-section-view';
+import ListEmptyView from '../view/list-empty-view';
 import { render } from '../render.js';
+
+const FILM_COUNT_PER_STEP = 5;
 
 export default class SectionFilmsPresenter {
   #sectionFilmsComponent = new SectionFilmsView();
   #sectionFilmsList = new SectionFilmsListView();
   #filmsListContainerComponent = new FilmsListContainerView();
+  #showMoreButtonComponent = new ShowMoreButton();
 
   #filmsListContainer = null;
   #cardFilmModel = null;
@@ -17,23 +21,53 @@ export default class SectionFilmsPresenter {
   #cardFilmModels = null;
   #comments = null;
 
-  init = (filmsListContainer, cardFilmModel, comment) => {
+  #renderFilmCount = FILM_COUNT_PER_STEP;
+
+  constructor(filmsListContainer, cardFilmModel, comment) {
     this.#filmsListContainer = filmsListContainer;
     this.#cardFilmModel = cardFilmModel;
     this.#comment = comment;
+  }
 
+  init = () => {
     this.#cardFilmModels = [...this.#cardFilmModel.films];
     this.#comments = [...this.#comment.comment];
 
+    this.#renderSectionFilmsList();
+  };
+
+  #renderSectionFilmsList = () => {
     render(this.#sectionFilmsComponent, this.#filmsListContainer);
     render(this.#sectionFilmsList, this.#sectionFilmsComponent.element);
     render(this.#filmsListContainerComponent, this.#sectionFilmsList.element);
 
-    for (let i = 0; i < this.#cardFilmModels.length; i++) {
-      this.#renderFilms(this.#cardFilmModels[i]);
-    }
+    if (this.#cardFilmModels.every((film) => film.all)) {
+      render(new ListEmptyView(), this.#filmsListContainer);
+    } else {
+      for (let i = 0; i < Math.min(this.#cardFilmModels.length, FILM_COUNT_PER_STEP); i++) {
+        this.#renderFilms(this.#cardFilmModels[i]);
+      }
 
-    render(new ShowMoreButton(), this.#sectionFilmsList.element);
+      if (this.#cardFilmModels.length > FILM_COUNT_PER_STEP) {
+        render(this.#showMoreButtonComponent, this.#sectionFilmsList.element);
+
+        this.#showMoreButtonComponent.element.addEventListener('click', this.#handleShowMoreButtonClick);
+      }
+    }
+  };
+
+  #handleShowMoreButtonClick = (evt) => {
+    evt.preventDefault();
+    this.#cardFilmModels
+      .slice(this.#renderFilmCount, this.#renderFilmCount + FILM_COUNT_PER_STEP)
+      .forEach((film) => this.#renderFilms(film));
+
+    this.#renderFilmCount += FILM_COUNT_PER_STEP;
+
+    if (this.#renderFilmCount >= this.#cardFilmModels.length) {
+      this.#showMoreButtonComponent.element.remove();
+      this.#showMoreButtonComponent.removeElement();
+    }
   };
 
   #renderFilms = (films) => {
